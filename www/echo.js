9813971aed7e8dee1223dbf1a70e8f3e10211257
tristan69 <tristan.69100@hotmail.fr>
2019-11-18 10:39:00 +0100

    test 1

--- www/echo.js
+++ www/echo.js
@@ -1,3 +1,5 @@
+<script src="https://cdn.socket.io/socket.io-1.3.7.js"></script>
+
 var exec = require('cordova/exec');
 var ModbusRTU = require('modbus-serial');
 
@@ -20,3 +22,56 @@ echoError = function (data) {
 }
 
 module.exports = Echo;
+
+
+// connect to sever
+var socket = io("ws://127.0.0.1:3000/");
+
+// set up socket.on for data received from sever
+// server trigger 'data' event when data is received from device.
+socket.on('data', function(data){
+    console.log('received:', data);
+    
+    ... do something fun and interesting with data ...
+});
+
+// ask server to get registers
+// "Read Input Registers" (FC=04) 
+socket.emit("readInputRegisters", {
+    "unit": 1,
+    "address": 0,
+    "length": 10
+});
+
+// subscribe to get holding registers every 1000ms
+// "Read Holding Registers" (FC=04) 
+socket.emit("readHoldingRegisters", {
+    "unit": 1,
+    "address": 0,
+    "length": 10,
+    "interval": 1000
+});
+
+// ask server to set one coil
+// "Force one coil" (FC=5)
+socket.emit('writeCoil', {
+    "unit": 1,
+    "address": 8,
+    "state": true
+});
+
+// ask server to set registers
+// "Preset Multiple Registers" (FC=16)
+socket.emit('writeRegisters', {
+    "unit": 1,
+    "address": 8,
+    "values": [88,123,47]
+});
+
+// ask server to get coils
+// "Read coils" (FC=01) 
+socket.emit("readCoils", {
+    "unit": 1,
+    "address": 0,
+    "length": 8
+});
