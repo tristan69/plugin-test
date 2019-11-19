ce17af5439ae9d7daae68bd8c5c750bf5c52a7e2
tristan69 <tristan.69100@hotmail.fr>
2019-11-19 09:34:04 +0100

    try to build jar

--- www/echo.js
+++ www/echo.js
@@ -1,33 +1,11 @@
 var exec = require('cordova/exec');
-//var SerialPort = require("serialport");
-//var ModbusRTU = require("modbus-serial");
 
 var Echo = function () {};
 
 Echo.echo = function (message) {
     exec(echoSuccess, echoError, "Echo", "echo", [message]);
 };
-/*
-Echo.test = function (){
-	
-	window.alert("DEBUT");
-	
-	window.alert("Serialport : " SerialPort);
-	
-	window.alert("modbusserial : " ModbusRTU);
-	
-	window.alert("DEBUT Serialport");
-	var serialPort = new SerialPort("/dev/ttyUSB0", {baudrate: 9600, autoOpen: false});
-	
-	window.alert("DEBUT modbus serial");
-	var client = new ModbusRTU(serialPort);
-	
-	window.alert("DEBUT open");
-	client.open();
-	
-	window.alert("FIN");
-};
-*/
+
 echoSuccess = function (data) {
 	window.alert("OK : " + data)
 }
