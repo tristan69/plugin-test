da9201082d70aa84ab5dfe26dc05649a90276f79
tristan69 <tristan.69100@hotmail.fr>
2019-11-19 15:53:53 +0100

    msldkfjqmlsdfkjmqlskdfj

--- src/android/Echo.java
+++ src/android/Echo.java
@@ -7,12 +7,24 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
+
 public class Echo extends CordovaPlugin {
 	
 @Override
 public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		if (action.equals("echo")){
 		try {
+			
+		ModbusTCPMaster master;
+		try {
+			master = new ModbusTCPMaster("", "", "");
+			master.connect();
+		}
+		catch (Exception e) {
+			logger.error("Cannot connect to slave - %s", e.getMessage());
+		}				
+			
 			String responseText = "Echo world, " + args.getString(0);
 			callbackContext.success(responseText);
 		} catch (JSONException e){
